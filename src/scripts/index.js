c5e42dc8fbce2f1cf07779702662b96794955d5b
Clara Miralles <clara.miralles.imperial@gmail.com>
2019-07-08 17:06:16 +0200

    webpack starter, basic html, css & js implemented. deleted docs, npm run build and mv build docs to publish in github pages

---
+++ src/scripts/index.js
@@ -0,0 +1,60 @@
+import '../styles/index.scss';
+
+const data = {
+    "version": "1.0",
+    "food": [
+      { "id": 1,
+        "name": "artichoke",
+        "image": '../images/artichoke.png'
+      },
+  
+      {
+        "id": 2,
+        "name": "baba_ganoush",
+        "image": '../images/baba_ganoush.png',
+      },
+  
+      {
+        "id": 3,
+        "name": "bean",
+        "image": '../images/bean.png',
+      },
+  
+      {
+        "id": 4,
+        "name": "cheese",
+        "image": '../images/cheese.png',
+      },
+  
+      {
+        "id": 5,
+        "name": "cream_cheese",
+        "image": '../images/cream_cheese.png',
+      },
+  
+      {
+        "id": 6,
+        "name": "guacamole",
+        "image": '../images/guacamole.png',
+      },
+  
+      {
+        "id": 7,
+        "name": "hummus",
+        "image": '../images/hummus.png',
+      },
+  
+      {
+        "id": 8,
+        "name": "pate",
+        "image": '../images/pate.png',
+      },
+  
+      {
+        "id": 9,
+        "name": "pesto",
+        "image": '../images/pesto.png',
+      }
+  
+    ]
+  };
